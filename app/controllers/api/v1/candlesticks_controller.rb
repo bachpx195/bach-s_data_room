c825820bda7e2edbe53a80d756c84f950b3a7566
Bach Pham <bachpx@ominext.com>
2025-03-17 14:21:18 +0700

    wk

--- app/controllers/api/v1/candlesticks_controller.rb
+++ app/controllers/api/v1/candlesticks_controller.rb
@@ -51,6 +51,7 @@ class Api::V1::CandlesticksController < Api::V1::BaseApiController
       result[:create_candlestick] = CandlestickServices::CreateService.new(params["merchandise_rate_ids"]).execute
       result[:create_metric_date] = CandlestickServices::CreateMetricDateService.new(params["merchandise_rate_ids"]).execute
       result[:create_label] = CandlestickServices::CreateLabelService.new.execute
+      result[:create_range] = CandlestickServices::CreateRangeService.new.execute
     end
 
     render json: result
